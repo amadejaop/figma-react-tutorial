71e63295d26d463158d4b382367abf41d8fa9cf7
Amadeja <obsteter.amadeja@gmail.com>
2024-02-02 05:37:10 +0100

    Add a bar chart
    Added a bar chart to the page and styled it to change colors on hover.

--- src/pages/expenses/expenses.tsx
+++ src/pages/expenses/expenses.tsx
@@ -3,27 +3,174 @@ import personOne from '../../assets/png/person1.png';
 import personTwo from '../../assets/png/person2.png';
 import personThree from '../../assets/png/person3.png';
 import addIcon from '../../assets/png/addIcon.png';
+import { BarChart, Bar, ResponsiveContainer, Cell } from 'recharts';
+import { useState } from 'react';
+
 
 export default function Expenses() {
-    return (
-        <>
-        <main className={styles.expenses}>
-            <div className={styles.expenseCard}>
-                <section className={styles.expensesOverview}>
-                    <div className={styles.expensesHeader}>
-                        <p className={styles.expensesTitle}>Expenses</p>
-                        <div className={styles.expensesActions}>
-                            <div className={styles.personImages}>
-                                <img src={personOne} alt="person one" /><img src={personTwo} alt="person two" /><img src={personThree} alt="person three" />
-                            </div>
-                            <button>
-                                <img src={addIcon} alt="add" />
-                            </button>
-                        </div>
-                    </div>
-                </section>
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const data = [
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
+
+  const onMouseOver = (data: any, index: number) => setActiveIndex(index);
+
+  return (
+    <>
+      <main className={styles.expenses}>
+        <div className={styles.expenseCard}>
+          <section className={styles.expensesOverview}>
+            <div className={styles.expensesHeader}>
+              <p className={styles.expensesTitle}>Expenses</p>
+              <div className={styles.expensesActions}>
+                <div className={styles.personImages}>
+                  <img src={personOne} alt="person one" /><img src={personTwo} alt="person two" /><img src={personThree} alt="person three" />
+                </div>
+                <button>
+                  <img src={addIcon} alt="add" />
+                </button>
+              </div>
             </div>
-        </main>
-        </>
-    )
+
+            <p className={styles.dateRange}>01 - 25 March, 2023</p>
+            <ResponsiveContainer width="100%" minHeight="9vh">
+              <BarChart width={150} height={40} data={data}>
+                <Bar dataKey="uv" fill="rgba(21, 122, 255, 0.2)" onMouseOver={onMouseOver}>
+                  {data.map((entry, index) => 
+                    <Cell cursor="pointer" fill={index === activeIndex ? "rgb(21, 122, 255)" : "rgba(21, 122, 255, 0.2)"} key={index}/>
+                  )}
+                </Bar>
+              </BarChart>
+            </ResponsiveContainer>
+          </section>
+        </div>
+      </main>
+    </>
+  )
 }
